809ddff0eb130889634a8d06369040494b0ec379
wavemoroc <patipan.wavemoroc@mail.kmutt.ac.th>
2021-08-07 14:12:14 +0700

    Change Title

--- subject-reservation-web-demo/src/views/Home.vue
+++ subject-reservation-web-demo/src/views/Home.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <br />
     <p style="font-family: Roboto; font-size: 18pt">
-      SIT Subject Reservation Web by wavemoroc001
+      SIT Subject Reservation Web by wavemoroc001 on netlify
     </p>
     <br />
     <v-row justify="center" align="top" no-gutters style="height: 150px">
